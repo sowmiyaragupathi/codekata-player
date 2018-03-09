bfec054445654212ff41ee69397394986f7ad45e
sowmiyaragupathi <34641629+sowmiyaragupathi@users.noreply.github.com>
2018-03-09 09:16:49 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,12 @@
+//factorial of a number
+#include<stdio.h>
+ int main(void) {
+   int n,i,fact=1;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+   	fact=fact*i;
+   }
+  printf("%d",fact);
+   return 0;
+}
